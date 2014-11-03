b22ea512566a8653d20c734fd2c040c1c6c5973f
ASNA ANSARI <asna.ansari@gmail.com>
2014-11-03 15:06:03 -0800

    changed quora scraper to be a well-documented and commented function

--- quora_scrape.py
+++ quora_scrape.py
@@ -1,37 +1,37 @@
-# Hard code in my username and password for expediency -- FIX ME: change this so that it accepts any user's login info
-
-first_name = 'Asna'
-last_name = 'Ansari'
-user_email = 'asna.ansari@gmail.com'
-user_password = 'asna1005'
-
-
-# Construct the URL of a user's profile page using their name
-url = 'http://www.quora.com/'+first_name+'-'+last_name
 
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Chrome(executable_path=r"/Users/Asna/Downloads/chromedriver")
 
-driver.get(url)
+def quora_crawl(first_name, last_name, user_email, user_password):
+	'''
+	INPUT: first and last name, user email and password for Quora account.
 
-# Find the username login form on the Quora login page
-form = driver.find_element_by_class_name('regular_login')
-username = form.find_element_by_name('email')
-username.send_keys(user_email)
-#time.sleep(60)
+	Uses the selenium webdriver to opens Chrome and navigates to user's profile page to scrape the content.
+	'''
 
-# Look for the password field in the Quora 
-password = form.find_element_by_name('password')
+	# Construct the URL of a user's profile page using their name
+	url = 'http://www.quora.com/'+first_name+'-'+last_name
+	driver = webdriver.Chrome(executable_path=r"/Users/Asna/Downloads/chromedriver")
 
-password.send_keys(user_password)
-password.send_keys(Keys.RETURN)
+	driver.get(url) # Navigate to the desired URL
 
-#time.sleep(60)
+	# Find the username login form on the Quora login page
+	form = driver.find_element_by_class_name('regular_login')
+	username = form.find_element_by_name('email')
+	username.send_keys(user_email)
+	#time.sleep(60)
 
-# UPDATE does seem to work with user-supplied inputs, albeit very slowly
+	# Look for the password field in the Quora 
+	password = form.find_element_by_name('password')
+	password.send_keys(user_password)
+	password.send_keys(Keys.RETURN)
 
-#driver.close();
+if __name__ == "__main__":
+	first_name = 'Asna'
+	last_name = 'Ansari'
+	user_email = 'asna.ansari@gmail.com'
+	user_password = 'asna1005'
+	quora_crawl(first_name, last_name, user_email, user_password)
