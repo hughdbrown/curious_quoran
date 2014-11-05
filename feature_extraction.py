e2d7f091394e3051872d65cddbb049957b7ce94e
ASNA ANSARI <asna.ansari@gmail.com>
2014-11-05 10:33:03 -0800

    starting feature extraction script

---
+++ feature_extraction.py
@@ -0,0 +1,12 @@
+import nltk
+from sklearn.feature import tfidf
+
+
+def get_features(*args):
+	'''
+	INPUT: an arbitrary number of arbitrarily long list of strings from various sources to be considered 
+	OUTPUT: TF-IDF vectorized documents to be used in similarity engine
+
+	'''
+
+
