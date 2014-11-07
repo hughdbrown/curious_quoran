00b6441d38765518b32e8e1d277f444e4900793d
ASNA ANSARI <asna.ansari@gmail.com>
2014-11-07 15:04:21 -0800

    added new gutenberg book dump func

--- parser.py
+++ parser.py
@@ -15,6 +15,7 @@ class TextParser():
     def __init__(self):
         '''
         Only attribute is a master dataframe with all ref texts
+        Cols of the dataframe: verbose description, clean description
         '''
         self.df = None
 
@@ -66,6 +67,33 @@ class TextParser():
         return book_df
 
 
+    def gutenberg_cat(path):
+        '''
+        INPUT: filepath to csv from Gutenberg SQL dump
+        OUTPUT: dataframe with Title, Author, subject tags and pre-vectorized descriptions
+
+        '''
+        ebook = pd.read_csv(path)
+
+        # Filter for only English books 
+        english_only = ebook[ebook['flanguage']=='{en}']
+        df = english_only[['ftitle', 'fsubjectname', 'ffriendlytitle']]
+
+        # Rename columns
+        df.columns = ['title', 'tags', 'title_auth']
+        df = df.dropna()
+
+        # Clean up cols
+        for col in df.columns:
+            df[col] = df[col].apply(lambda x: x.replace('{', '')).apply(lambda x: x.replace('}', '')).apply(lambda x: x.replace('"', ''))
+
+        df.subj_tags = df_new.subj_tags.apply(lambda x: x.replace('--',''))
+
+        # Make a composite description column to be cleaned up
+
+        df['desc_tot'] = df['subj_tags']+' '+ df['Title']
+        return df
+        
     def assemble_df(self):
         '''
         INPUT: dataframe from gutenberg
