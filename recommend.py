39665d8797f5dce439347b579a4738324ec94712
ASNA ANSARI <asna.ansari@gmail.com>
2014-11-10 12:16:23 -0800

    updates to recommender

--- recommend.py
+++ recommend.py
@@ -1,17 +1,16 @@
 import pickle
 import pandas as pd
-from string import punctuation
-from nltk.tokenize import word_tokenize
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.spatial.distance import cosine
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
 class Recommender():
     '''
+    
     Recommender class with methods that generate most (cosine) similar resources for a given block of Quora text
+    
     '''
 
     def __init__(self):
@@ -19,11 +18,14 @@ class Recommender():
         Only attribute is a master dataframe with all ref texts
         Cols of the dataframe: verbose description, clean description
         '''
-        self.df = pickle.load(open("master_df.pkl"))
-        self.quora = pickle.load(open("clean_quora.pkl"))
-        self.distances = None
+        self.df = pickle.load(open("data/master_df.pkl"))
+        self.quora = pickle.load(open("data/clean_quora.pkl"))
+        self.quora = pd.Series(self.quora)
+        print "The quora data as it is read in", self.quora
+        self.distances = []
+        print self.df
 
-    def vectorize(self, list):
+    def vectorize(self):
         '''
         INPUT: list of cleaned, lemmatized and filtered text
         OUTPUT: cleaned up string ready to be fed into a vectorizer
@@ -31,11 +33,20 @@ class Recommender():
         Stem, lemmatize words in list and strip non alphabet chars and stopwords 
         '''
         vec = TfidfVectorizer()
-        doc_vecs = [vec.fit_transform(doc) for doc in df]
-        quora_vec = vec.transform(self.quora)
+        doc_vecs_sparse = vec.fit_transform(self.df.desc.values)
+        doc_vecs = doc_vecs_sparse.toarray()
+        quora_vec = vec.transform(self.quora.values)
 
-        # Get pairwise distances using pdist, rank and argsort
-        self.distances = "something"
+        print "Shape of doc vector array", doc_vecs.shape
+        print "Shape of quora vector", quora_vec.shape
+        
+        for doc in doc_vecs:
+            doc = doc.reshape(1,doc.shape[0])
+            print doc.shape
+            print quora_vec.shape
+            self.distances.append(cosine(quora_vec, doc))
+
+        #self.distances = [cosine(quora_vec, d) for d in doc_vecs]
 
 
     def recommend(self):
@@ -48,13 +59,10 @@ class Recommender():
         return top_ten
 
 
-
-
-
 if __name__ == "__main__":
 
     rec = Recommender()
-    top_ten_ind = rec.recommend()
-    recs = read.df.ix[top_ten_ind]
-    return recs[['title', 'type']]
+    test = rec.vectorize()
+    # recs = read.df.ix[top_ten_ind]
+    # return recs[['title', 'type']]
 
