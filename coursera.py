b9ec11dfb874eafbb7a7fc50e462a7be3a708949
ASNA ANSARI <asna.ansari@gmail.com>
2014-11-11 11:24:10 -0800

    cleaned up coursera module

--- coursera.py
+++ coursera.py
@@ -1,15 +1,12 @@
 import pandas as pd
-from bs4 import BeautifulSoup
 import requests
-import re
-import pickle
 
 def get_coursera_data():
     '''
     INPUT: dict from FeedWrangler API
     OUTPUT: dict with title, description for top 50 most popular podcasts
 
-    Accesses the Coursera course catalog
+    Accesses the Coursera course catalog and assembles dataframe with course title and description
     '''
 
     courses = requests.get('https://api.coursera.org/api/catalog.v1/courses')
@@ -19,10 +16,8 @@ def get_coursera_data():
     df = pd.DataFrame.from_dict(course_dict)
     df['type']= "course"
     df = df[['name', 'shortName', 'type']]
-    df['desc'] = df['name']+' '+df['shortName']
-    df.columns = ['title', 'desc1', 'type', 'desc']
-    del df['desc1']
-    df = df[['title', 'desc', 'type']]
+    df['shortName'] = df['name']+' '+df['shortName']
+    df.columns = ['title', 'desc', 'type']
     print "There are {0} courses in the catalog.".format(len(df))
     return df
 
