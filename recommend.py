f98a4f570da4e5eeeb011b49e893e2530146df09
Asna Ansari <asna.ansari@gmail.com>
2014-11-11 14:06:07 -0800

    cleaning up recommender class

--- recommend.py
+++ recommend.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from scipy.spatial.distance import cosine
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -21,12 +20,9 @@ class Recommender():
         Cols of the dataframe: verbose description, clean description
         '''
         self.df = pickle.load(open("data/master_df.pkl"))
-        self.quora = pickle.load(open("data/clean_quora.pkl"))
-        self.quora = pd.Series(self.quora)
-        print "The quora data as it is read in", self.quora
+        self.quora = pd.Series(pickle.load(open("data/clean_quora.pkl")))
         self.distances = []
-        print self.df
-
+        
     def vectorize(self):
         '''
         INPUT: list of cleaned, lemmatized and filtered text
@@ -36,7 +32,7 @@ class Recommender():
         with a user's Quora profile information or Question Page 
         '''
 
-        vec = TfidfVectorizer(ngram_range = (1,4), max_features = 4000)
+        vec = TfidfVectorizer(ngram_range = (1,5), max_features = 6000)
         doc_vecs_sparse = vec.fit_transform(self.df.desc.values)
         quora_vec = vec.transform(self.quora.values)
         self.distances = cosine_similarity(quora_vec, doc_vecs_sparse)[0]
