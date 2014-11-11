94b456979e09d298b4da8933fb0eaca56b676bc7
ASNA ANSARI <asna.ansari@gmail.com>
2014-11-11 11:36:41 -0800

    cleaned up podcast description aggregator

--- podcasts.py
+++ podcasts.py
@@ -1,13 +1,12 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-import re
 import pickle
 
 def get_podcast_data():
     '''
     INPUT: dict from FeedWrangler API
-    OUTPUT: dict with title, description for top 50 most popular podcasts
+    OUTPUT: DF with title, description for top 50 most popular podcasts
 
     Accesses a list of the top 50 podcasts via FeedWrangler API, loops over ids and obtains descriptions with another API call
     '''
@@ -15,30 +14,28 @@ def get_podcast_data():
     top_fifty = requests.get('https://feedwrangler.net/api/v2/podcasts/popular')
     master_dict = top_fifty.json()
     podcast_dict = master_dict["podcasts"]
-
     podcast_df = pd.DataFrame.from_dict(podcast_dict)
     return podcast_df
 
 
 def pcast_desc(rss_url):
+    '''
+    INPUT: URL of RSS episode feed for each podcast 
+    OUTPUT: aggregate text from all recent episode descriptions
 
+    Collects and cleans episode descriptions from podcast episodes using BeautifulSoup
+    '''
     description = requests.get(rss_url).text
-   
-    # Parse the page with BeautifulSoup
     soup = BeautifulSoup(description, 'xml')
-    summary_text = str(soup.findAll('summary'))
-    
+    summary_text = str(soup.findAll('summary')) # Pick out episode summaries only
     text = soup.get_text()
     clean = summary_text.replace('\n', '').replace('<itunes:summary>','').replace('</itunes:summary>','')
     return clean
 
 
-
 if __name__=="__main__":
     df = get_podcast_data()
-    df['desc'] = df['feed_url'].apply(lambda x: pcast_desc(x))
+    df['desc'] = df['feed_url'].apply(lambda x: pcast_desc(x)) # Collect descriptions using feed urls
     df = df[['title', 'desc']]
     df.desc = df['desc'].apply(lambda x: x.replace('[','')).apply(lambda x: x.replace('[',''))
-
-    # Dump raw data to be processed by parser class
-    pickle.dump(df, open("data/podcast_df.pkl", "wb"))
+    pickle.dump(df, open("data/podcast_df.pkl", "wb")) # Dump raw data to be processed by parser class
