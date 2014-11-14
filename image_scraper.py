172b3d629670ed36dcb7e2886fb1bfa0f0e50bae
Asna Ansari <asna.ansari@gmail.com>
2014-11-13 18:45:40 -0800

    cleaned up image scraper

--- image_scraper.py
+++ image_scraper.py
@@ -13,22 +13,13 @@ def get_image(searchTerm):
     searchTerm = searchTerm.replace('%','')
     searchTerm = searchTerm.replace(' ','%20')
     
-    count= 0
-    urls = []
-    for i in range(0,2):
-        # Notice that the start changes for each iteration in order to request a new set of images for each loop
-        url = ('https://ajax.googleapis.com/ajax/services/search/images?' + 'v=1.0&q='+searchTerm+'&start='+str(i*4)+'&userip=MyIP')
-        print "search url: \n", url
-        request = urllib2.Request(url, None, {'Referer': 'testing'})
-        response = urllib2.urlopen(request)
+    url = ('https://ajax.googleapis.com/ajax/services/search/images?' + 'v=1.0&q='+searchTerm+'&start='+str(0)+'&userip=MyIP')
+    request = urllib2.Request(url, None, {'Referer': 'testing'})
+    response = urllib2.urlopen(request)
+    results = simplejson.load(response)
+    data = results['responseData']
+    dataInfo = data['results']
 
-        results = simplejson.load(response)
-        data = results['responseData']
-        dataInfo = data['results']
-
-        for myUrl in dataInfo:
-            count += 1
-            urls.append(myUrl['unescapedUrl'])
-
-        time.sleep(1)
-        return urls
+    img_urls = [myUrl['unescapedUrl'] for myUrl in dataInfo]
+    
+    return img_urls
