6a134541c6e9c3452fea5cf0c2a3bc24b203e4f2
Asna Ansari <asna.ansari@gmail.com>
2014-11-17 21:55:17 -0800

    updates to app, revamping rec landing page

--- app.py
+++ app.py
@@ -7,12 +7,23 @@ from image_scraper import get_image
 from quora_scrape import profile_crawl
 app = Flask(__name__)
 
+df = pickle.load(open("data/recs.pkl", "rb"))
 master_df = pickle.load(open('data/master_df.pkl'))
+
+
+# @app.route('/testrecs')
+# def testing():
+# 	return render_template('testing.html', data = df[['title', 'img_link']])
 
 
 @app.route('/')
 def submit_quora():
 	return render_template('submit.html')
+
+@app.route('/recs')
+def rec():	
+	return render_template('testing.html', data = df[['title', 'img_link']].values)
+
 
 @app.route('/recommend', methods=['POST'])
 def recommend():
@@ -35,9 +46,8 @@ def recommend():
 	recs = recs.reset_index()
 	recs['img_link'] = map(get_image, recs['title'])
 	recs.loc[recs['type']=='course', 'img_link'] = 'http://www.michaellenox.com/wp-content/uploads/2014/07/coursera_square_logo.jpg'
-	recs = recs[0:5]
-	recs['div_tag'] = ['ONE', 'TWO', 'THREE', 'FOUR', 'FIVE']
-	return render_template('index.html', data = recs[['title', 'img_link', 'div_tag']].values)
+	recs = recs[0:20]
+	return render_template('testing.html', data = recs[['title', 'img_link']].values)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=7725, debug=True)
+	app.run(host='0.0.0.0', port=8311, debug=True)
